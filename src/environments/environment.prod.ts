ffd71ef816df973bfd1580c7a2551a6e214e2c22
Luciano <llutti@users.noreply.github.com>
2018-08-25 11:53:10 -0300

    Configuração do Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDZD0KMJ0jaEMoLJ8-mTrDarSuIvWXj7jc",
+    authDomain: "loginapp-681a6.firebaseapp.com",
+    databaseURL: "https://loginapp-681a6.firebaseio.com",
+    projectId: "loginapp-681a6",
+    storageBucket: "loginapp-681a6.appspot.com",
+    messagingSenderId: "794893028687"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDZD0KMJ0jaEMoLJ8-mTrDarSuIvWXj7jc",
+    authDomain: "loginapp-681a6.firebaseapp.com",
+    databaseURL: "https://loginapp-681a6.firebaseio.com",
+    projectId: "loginapp-681a6",
+    storageBucket: "loginapp-681a6.appspot.com",
+    messagingSenderId: "794893028687"
+  }
 };
 
 /*
