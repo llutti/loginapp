88708a1006bdd1f28d98011097b94f64617c5a79
Luciano <llutti@users.noreply.github.com>
2018-08-25 21:57:21 -0300

    Adicionada a funcionalidade de login e logout

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicos/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  private email: string;
+  private password: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  onSubmitLogin() {
+    console.log('Executar onSubmitLogin');
+
+    this.authService.loginEmail(this.email, this.password)
+      .then((res) => {
+        console.log('Sucesso!!', res);
+        this.router.navigate(['/privado']);
+      })
+      .catch((err) => {
+        console.log('Erro!!', err);
+        // this.router.navigate(['/login']);
+      });
+  }
 }
--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicos/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  private isLogin: boolean;
+  private userName: string;
+  private userEmail: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.userName = auth.displayName;
+        this.userEmail = auth.email;
+      }
+      else {
+        this.isLogin = false;
+        this.userName = '';
+        this.userEmail = '';
+      }
+    })
+  }
+
+  onClickLogout() {
+    this.authService.logout();
   }
 
 }
